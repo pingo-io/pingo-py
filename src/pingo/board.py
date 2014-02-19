e046263d7c7a2756bf1b359381dbe13b933f6cdc
Luciano Ramalho <luciano@ramalho.org>
2014-02-19 18:33:43 -0300

    refactoring to remove properties

--- src/pingo/board.py
+++ src/pingo/board.py
@@ -1,6 +1,46 @@
 
+HIGH = 1
+LOW = 0
+INPUT = 0
+OUTPUT = 1
+
 class Board(object):
 	pass
+
+class Pin(object):
+
+    def __init__(self, physical_pin):
+        self.physical_pin = physical_pin
+
+    def __repr__(self):
+        return '<%s #%d>' % (
+            self.__class__.__name__,
+            self.physical_pin)
+
+class GroundPin(Pin):
+    pass
+
+class VddPin(Pin):
+    pass
+
+class DigitalPin(Pin):
+
+    def __init__(self, physical_pin, mode=OUTPUT, state=LOW):
+        Pin.__init__(self, physical_pin)
+        self.set_mode(mode)
+        self.state = state
+
+    def set_mode(self, mode):
+        self.mode = mode
+
+    def low(self):
+        self.state = LOW
+
+    def high(self):
+        self.state = HIGH
+
+
+
 
 INPUT = 0
 OUTPUT = 1
--- src/pingo/tests/rpi.rst
+++ src/pingo/tests/rpi.rst
@@ -7,7 +7,8 @@ Turn on a led for 1s
 
 ::
 
-	>>> from time import sleep
+	>>> # from time import sleep
+	>>> sleep = lambda x: None
 	>>> import pingo
 	>>>
 	>>> board = pingo.rpi.RaspberryPi()
@@ -15,11 +16,11 @@ Turn on a led for 1s
 	<DigitalPin #11>
 	>>>
 	>>> led_pin = board.pins[11]
-	>>> led_pin.mode = pingo.OUTPUT
-	>>> led_pin.state = 1
+	>>> led_pin.set_mode(pingo.OUTPUT)
+	>>> led_pin.high()
 	>>> led_pin.state
 	1
 	>>> sleep(1)  # 1 second
-	>>> led_pin.state = 0
+	>>> led_pin.low()
 	>>> led_pin.state
 	0
