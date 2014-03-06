f586faa93ca051675b0c652670374166ff65a57a
Ektor5 <ektor-5@udoo.org>
2014-03-05 23:57:01 -0300

    basic UDOO test passing

--- src/pingo/board.py
+++ src/pingo/board.py
@@ -6,26 +6,31 @@ OUTPUT = 1
 
 class Board(object):
 	
-	def add_pins(self, pin_map):
-		""" pin_map is a list of pairs: [(pin_location, pin_instance), ...]
-		    where pin_location is a physical pin id and pin_instance is
-		    an instance of Pin
-		"""
-		self.pins = {}
-		for pin_location, pin in pin_map:
-			pin.board = self
-			self.pins[pin_location] = pin
+    def add_pins(self, pin_map):
+        """ pin_map is a list of pairs: [(pin_location, pin_instance), ...]
+            where pin_location is a physical pin id and pin_instance is
+            an instance of Pin
+        """
+        self.pins = {}
+        for pin_location, pin in pin_map:
+            if pin is not None:
+                pin.board = self
+                self.pins[pin_location] = pin
 		
+    def set_pin_mode(self, pin, mode):        
+        raise NotImplementedError
+        
 class Pin(object):
 
-    def __init__(self, logical_pin):
+    def __init__(self, board, logical_pin):
+        self.board = board
         self.logical_pin = logical_pin
 
     def __repr__(self):
         return '<%s %r>' % (
             self.__class__.__name__,
             self.logical_pin)
-
+            
 class GroundPin(Pin):
     pass
 
@@ -34,18 +39,21 @@ class VddPin(Pin):
 
 class DigitalPin(Pin):
 
-    def __init__(self, logical_id, mode=OUTPUT, state=LOW):
-        Pin.__init__(self, logical_id)
+    def __init__(self, board, logical_id, mode=OUTPUT, state=LOW):
+        Pin.__init__(self, board, logical_id)
         self.set_mode(mode)
         self.state = state
 
     def set_mode(self, mode):
+        self.board.set_pin_mode(self, mode)
         self.mode = mode
 
     def low(self):
+        self.board.set_pin_state(self, LOW)
         self.state = LOW
 
     def high(self):
+        self.board.set_pin_state(self, HIGH)
         self.state = HIGH
 
 
--- src/pingo/udoo/__init__.py
+++ src/pingo/udoo/__init__.py
@@ -1,19 +1,71 @@
-from pingo.board import Board, DigitalPin
 
-# there are no gaps in the Arduino digital pin numbering of the 
-# Due and the Udoo
-# pin_list[arduino_pin] -> gpio_pin
+"""
+UDOO board
 
+Reference:
+http://www.udoo.org/ProjectsAndTutorials/linux-gpio-manipulation/
+"""
+
+import os
+
+from pingo.board import Board, DigitalPin, INPUT, OUTPUT
+
+# there are no gaps in the Arduino digital pin numbering 
+# of the Arduino Due embedded in the Udoo
+
+# pin_list[physical_arduino_pin] -> logical_gpio_pin
 pin_list = [
-    116, 112, 20, 16, 17, 18, 41, 42, 21, 19, 1, 9, 3, 40, 150, 
-    162, 160, 161, 158, 159, 92, 85, 123, 124, 125, 126, 127, 133, 
-    134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 54, 205, 32, 
-    35, 34, 33, 101, 144, 145, 89, 105, 104, 57, 56, 55, 88
+    116, 112, 20, 16, 17, 18, 41, 42, 21, 19, 1, 9, 3, 40, # <-- 13
+    150, 162, 160, 161, 158, 159, 92, 85, 123, 124, 125, 126, 127, 
+    133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 54, 205, 
+    32, 35, 34, 33, 101, 144, 145, 89, 105, 104, 57, 56, 55, 88
 ]
+
+# /sys/class/gpio/gpio40/ --> Arduino pin #13
+DIGITAL_PINS_PATH = '/sys/class/gpio'
+DIGITAL_PIN_MASK = 'gpio%d' 
+DIGITAL_PIN_STATE_FILENAME = 'value'
+DIGITAL_PIN_MODE_FILENAME = 'direction'
+DIGITAL_PIN_MODES = {INPUT: 'in', OUTPUT: 'out'}
 
 class Udoo(Board):
 
     def __init__(self):
-        self.add_pins((physical, DigitalPin(logical)) for physical, logical in enumerate(pin_list))
+        self.add_pins(self._build_pin_map())
         self.pin_path_mask = '/sys/class/gpio/gpio%d/'
         
+    def _build_pin_map(self):    
+        pairs = []
+        for physical, logical in enumerate(pin_list):
+            try:
+                self._base_pin_path(logical)
+            except IOError:
+                pin = None
+            else:
+                pin = DigitalPin(self, logical)
+            pairs.append((physical, pin))
+        return pairs
+        
+    def _base_pin_path(self, logical_pin):
+        path = os.path.join(DIGITAL_PINS_PATH, DIGITAL_PIN_MASK % logical_pin)
+        if not os.path.exists(path):
+            raise IOError('no such path: %r' % path)
+        return path 
+		
+    def _pin_mode_filename(self, logical_pin):
+        path = self._base_pin_path(logical_pin)
+        return os.path.join(path, DIGITAL_PIN_MODE_FILENAME)
+
+    def _pin_state_filename(self, logical_pin):
+        return os.path.join(self._base_pin_path(logical_pin), DIGITAL_PIN_STATE_FILENAME)
+
+    def set_pin_mode(self, pin, mode):
+        assert mode in DIGITAL_PIN_MODES, '%r not in %r' % (mode, DIGITAL_PIN_MODES)
+        with open(self._pin_mode_filename(pin.logical_pin), "wb") as fp:
+            fp.write(DIGITAL_PIN_MODES[mode])
+
+    def set_pin_state(self, pin, state):
+        with open(self._pin_state_filename(pin.logical_pin), "wb") as fp:
+            fp.write(str(state))
+		
+		
