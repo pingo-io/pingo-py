a6fe5d321cf67b408d921a65f87d6cc47f8d0383
Ektor5 <ektor-5@udoo.org>
2014-03-06 00:13:03 -0300

    first example: UDOO blink

---
+++ src/pingo/examples/udoo_blink.py
@@ -0,0 +1,23 @@
+### horrible hack: need to properly package and install pingo library
+import sys
+import os
+path = os.path.abspath(__file__)
+path = path[:path.rfind('pingo')]
+sys.path.append(path)
+### please ignore this line and all above it
+
+import pingo
+from time import sleep
+
+board = pingo.udoo.Udoo()
+led_pin = board.pins[13]
+led_pin.set_mode(pingo.OUTPUT)
+
+while True:
+    led_pin.high()
+    print '%r -> %r' % (led_pin, led_pin.state)
+    sleep(1)
+    led_pin.low()
+    print '%r -> %r' % (led_pin, led_pin.state)
+    sleep(1)
+    
