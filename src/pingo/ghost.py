731faed812362d1994d5eae4cf77b8ee60665bdc
Luciano Ramalho <luciano@ramalho.org>
2014-03-22 07:55:48 -0300

    minor fix to ghost.py

--- src/pingo/ghost.py
+++ src/pingo/ghost.py
@@ -3,10 +3,10 @@ from pingo.board import Board, DigitalPin
 class GhostBoard(Board):
 
     def __init__(self):
-		self.add_pins([(13, DigitalPin(self, 13))])
-		
+		self.add_pins({13: DigitalPin(self, 13)})
+
     def set_pin_mode(self, pin, mode):
         pass
-        
+
     def set_pin_state(self, pin, state):
         pass
