824094a0eb30b62126f323c1dda9a858e9a815e1
Vido <vido@usp.br>
2014-04-21 17:00:13 -0300

    Setable pin state in GhostBoard Now we can set states in GhostBoard: via a json file

--- pingo/ghost/ghost.py
+++ pingo/ghost/ghost.py
@@ -1,15 +1,24 @@
+import os
+import json
+
 import pingo
+
+PINS_STATE_FILEPATH = os.path.join(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        ), 'pins_state.json')
 
 class GhostBoard(pingo.Board):
 
     def __init__(self):
         super(GhostBoard, self).__init__()
 
-        pins = [
+        pins = set([
             pingo.GroundPin(self, 1),
             pingo.VddPin(self, 2, 5.0),
+            pingo.DigitalPin(self, 7),
             pingo.DigitalPin(self, 13),
-        ]
+        ])
 
         self.add_pins(pins)
 
@@ -20,7 +29,17 @@ class GhostBoard(pingo.Board):
         pass
 
     def _set_pin_state(self, pin, state):
-        pass
+        _state = 1 if state == pingo.HIGH else 0
+        with open(PINS_STATE_FILEPATH, 'r') as fp:
+            pins_state = json.load(fp)
+            pins_state[str(pin.location)] = _state
+
+        with open(PINS_STATE_FILEPATH, 'w') as fp:
+            print pins_state
+            json.dump(pins_state, fp, indent=4)
 
     def _get_pin_state(self, pin):
-        return pingo.LOW
+        with open(PINS_STATE_FILEPATH, 'r') as fp:
+            pins_state = json.load(fp)
+            state = pins_state[str(pin.location)]
+        return state
