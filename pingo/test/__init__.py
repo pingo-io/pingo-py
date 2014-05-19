0a8f303159b8ac1818ce38d84cfa3da039329e01
Valder Gallo <valdergallo@gmail.com>
2014-05-18 22:08:16 -0300

    fix not_has_module

--- pingo/test/__init__.py
+++ pingo/test/__init__.py
@@ -1,6 +1,10 @@
 import level0
 import level1
 
-def not_has_module(value):
-    from pip.util import get_installed_distributions
-    return (value not in [i.project_name for i in get_installed_distributions()])
+
+def not_has_module(module_name):
+    try:
+        __import__(module_name)
+        return False
+    except ImportError:
+        return True
