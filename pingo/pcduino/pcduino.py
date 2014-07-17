4eb9b31221419c623c705118b01dbd456c11136c
Vido <vido@usp.br>
2014-07-17 00:38:09 +0000

    Fixing V1 class atributes references in methods.

--- pingo/pcduino/pcduino.py
+++ pingo/pcduino/pcduino.py
@@ -17,22 +17,27 @@ class PcDuinoV1(Board, AnalogInputCapable):
     ANALOG_PIN_RESOLUTIONS = [6, 6, 12, 12, 12, 12]
 
     def __init__(self):
-        self._add_pins([DigitalPin(self, location)
-                       for location in range(LEN_DIGITAL_PINS)] +
-                      [AnalogPin(self, 'A%s' % location, resolution=bits)
-                       for location, bits in enumerate(ANALOG_PIN_RESOLUTIONS)])
+        self._add_pins(
+            [DigitalPin(self, location)
+                for location in range(self.LEN_DIGITAL_PINS)] +
+            [AnalogPin(self, 'A%s' % location, resolution=bits)
+                for location, bits in enumerate(self.ANALOG_PIN_RESOLUTIONS)])
 
     def _set_pin_mode(self, pin, mode):
-        assert mode in DIGITAL_PIN_MODES, '%r not in %r' % (mode, DIGITAL_PIN_MODES)
-        with open(DIGITAL_PINS_PATH+'mode/gpio%s' % pin.location, 'w') as fp:
-            fp.write(DIGITAL_PIN_MODES[mode])
+        err_msg = '%r not in %r' % (mode, DIGITAL_PIN_MODES)
+        assert mode in DIGITAL_PIN_MODES, err_msg
+        sys_string = self.DIGITAL_PINS_PATH+'mode/gpio%s' % pin.location
+        with open(sys_string, 'w') as fp:
+            fp.write(self.DIGITAL_PIN_MODES[mode])
 
     def _set_pin_state(self, pin, state):
-        with open(DIGITAL_PINS_PATH+'pin/gpio%s' % pin.location, 'w') as fp:
-            fp.write(DIGITAL_PIN_STATES[state])
+        sys_string = self.DIGITAL_PINS_PATH+'pin/gpio%s' % pin.location
+        with open(sys_string, 'w') as fp:
+            fp.write(self.DIGITAL_PIN_STATES[state])
 
     def _get_pin_state(self, pin):
-        with open(DIGITAL_PINS_PATH+'pin/gpio%s' % pin.location, 'r') as fp:
+        sys_string = self.DIGITAL_PINS_PATH+'pin/gpio%s' % pin.location
+        with open(sys_string, 'r') as fp:
             state = fp.read().strip()
             return HIGH if state == '1' else LOW
 
@@ -40,8 +45,8 @@ class PcDuinoV1(Board, AnalogInputCapable):
         pass
 
     def _get_pin_value(self, pin):
-        adc_id = pin.location[-1]
-        with open(ADC_PATH+'adc%s' % adc_id) as fp:
+        sys_string = self.ADC_PATH+'adc%s' % pin.location[1:] # eg. A5
+        with open(sys_string) as fp:
             fp.seek(0)
             return int(fp.read(16).split(':')[1])
 
