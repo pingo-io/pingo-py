51ff04f2eaaa088941f3b2e20247e805d3e8688f
Humberto Rocha <humrochagf@gmail.com>
2014-11-08 14:00:16 -0200

    Adding seven segment display part

--- pingo/parts/led.py
+++ pingo/parts/led.py
@@ -3,13 +3,16 @@ import pingo
 import time
 import threading
 
+
 class Led(object):
     """A single LED"""
 
     def __init__(self, pin, lit_state=pingo.HIGH):
-        """Set lit_state to pingo.LOW to turn on led by bringing cathode to low state.
+        """Set lit_state to pingo.LOW to turn on led by bringing
+           cathode to low state.
 
-        :param lit_state: use pingo.HI for anode control, pingo.LOW for cathode control
+        :param lit_state: use pingo.HI for anode control, pingo.LOW
+                          for cathode control
         """
 
         pin.mode = pingo.OUT
@@ -90,3 +93,21 @@ class BlinkTask(object):
         else:
             self.led.off()
             self.active = False
+
+
+DIGIT_MAP = (0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70,
+             0x7F, 0x7B, 0x77, 0x1F, 0x4E, 0x3D, 0x4F, 0x47)
+
+
+class SevenSegments(object):
+
+    def __init__(self, pin_a, pin_b, pin_c, pin_d,
+                 pin_e, pin_f, pin_g, pin_dp=None,
+                 lit_state=pingo.HIGH):
+        self._leds = [Led(pin_a, lit_state), Led(pin_b, lit_state),
+                      Led(pin_c, lit_state), Led(pin_d, lit_state),
+                      Led(pin_e, lit_state), Led(pin_f, lit_state),
+                      Led(pin_g, lit_state)]
+
+        if pin_dp:
+            self._leds.append(Led(pin_dp, lit_state))
