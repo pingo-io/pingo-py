ede6759e88cdeeda62ebb6635a1c67dc1532e01c
Davi Garcia <davivcgarcia@gmail.com>
2014-11-08 16:49:22 -0300

    MVP of SevenSegments and example script.

--- pingo/examples/parts/7seg_demo.py
+++ pingo/examples/parts/7seg_demo.py
@@ -1,3 +1,28 @@
 """ Seven segment display demo using the Garoa Dojo Shield """
 
+from time import sleep
+from pingo import OUT, detect, parts
 
+INTERVAL = 0.3
+
+ard = detect.MyBoard()
+
+display_pins = [ard.pins[i] for i in range(8, 14)] + [7]
+
+for pin in display_pins:
+    pin.mode = OUT
+
+seg_display = parts.led.SevenSegments(*display_pins)
+
+for num in range(16):
+    seg_display.digit = num
+    print('Current digit: {:x}'.format(seg_display.digit))
+    sleep(INTERVAL)
+
+for i in range(3):
+    seg_display.off()
+    sleep(INTERVAL)
+    seg_display.on()
+    sleep(INTERVAL)
+
+seg_display.off()
