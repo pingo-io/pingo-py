dcf4020496fee2631b18ce6a585d6c929fdc019d
Luciano Ramalho <luciano@ramalho.org>
2014-11-28 16:06:14 -0200

    pot example with text bar

--- experiments/galileo2/pot.py
+++ experiments/galileo2/pot.py
@@ -1,6 +1,9 @@
-﻿import mraa
+﻿from time import sleep
+import mraa
+
+MAX_VALUE = 2. ** 12 -1
 
 pot = mraa.Aio(0)
 while 1:
-	print pot.read()
-
+    print '#' * int(pot.read()/MAX_VALUE * 70)
+    sleep(.05)
