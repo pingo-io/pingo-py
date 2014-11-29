36e991344d5a89169a87ec3443f0fba6eb5039e7
Luciano Ramalho <luciano@ramalho.org>
2014-11-29 12:19:43 -0200

    dual_analog_bars.py

--- pingo/examples/dual_analog_bars.py
+++ pingo/examples/dual_analog_bars.py
@@ -1,21 +1,21 @@
 import pingo
 import time
 
-this = pingo.detect.MyBoard()
-that = pingo.arduino.get_arduino()
+galileo = pingo.detect.MyBoard()
+arduino = pingo.arduino.get_arduino()
 
-pot_this = this.pins['A0']
-pot_this.mode = pingo.ANALOG
+pot_galileo = galileo.pins['A0']
+pot_galileo.mode = pingo.ANALOG
 
-pot_that = that.pins['A0']
-pot_that.mode = pingo.ANALOG
+pot_arduino = arduino.pins['A0']
+pot_arduino.mode = pingo.ANALOG
 
 
 def bar(pin1, pin2):
     bar1 = ('*' * int(pin1.ratio() * 40)).ljust(40)
     bar2 = ('*' * int(pin2.ratio() * 40)).rjust(40)
-    print bar1 + bar2
+    print bar2 + bar1
 
 while True:
-    bar(pot_this, pot_that)
+    bar(pot_galileo, pot_arduino)
     time.sleep(0.05)
