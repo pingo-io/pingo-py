cb6ae77c4db27a4bf4ebafe216ec979461363be5
Luciano Ramalho <luciano@ramalho.org>
2014-11-29 13:45:05 -0200

    working on pong.py

--- pingo/examples/pong.py
+++ pingo/examples/pong.py
@@ -1,5 +1,5 @@
 import curses
-#import pingo
+import pingo
 import time
 import random
 
@@ -16,10 +16,10 @@ score_1 = 0
 score_2 = 0
 
 def init_ball():
-    return ([39, 12], 
+    return ([39, 12],
         random.choice([
-        #[1, 0], 
-        [1, 1], #[-1, 0], 
+        #[1, 0],
+        [1, 1], #[-1, 0],
         [-1, 1], [1, -1]]))
 
 def new_ball_pos(pos, velocity):
@@ -29,7 +29,7 @@ def new_ball_pos(pos, velocity):
 
 def paddle_collision(ball_pos, paddle_pos, paddle_size):
     return (ball_pos[0] == paddle_pos[0] and
-             paddle_pos[1] <= ball_pos[1] <= paddle_pos[1] + PADDLE_SIZE) 
+             paddle_pos[1] <= ball_pos[1] <= paddle_pos[1] + PADDLE_SIZE)
 
 def draw_paddle(x, y, color):
     for offset in range(PADDLE_SIZE):
@@ -52,9 +52,19 @@ if __name__ == '__main__':
 
     ball_pos, ball_velocity = init_ball()
 
+    galileo = pingo.detect.MyBoard()
+    arduino = pingo.arduino.get_arduino()
+
+    pot_galileo = galileo.pins['A0']
+    pot_galileo.mode = pingo.ANALOG
+
+    pot_arduino = arduino.pins['A0']
+    pot_arduino.mode = pingo.ANALOG
+
+
     while True:
-        paddle_1_pos = 10
-        paddle_2_pos = 10
+        paddle_1_pos = int(pot_arduino.ratio(to_min=MIN_Y, to_max=MAX_Y-PADDLE_SIZE))
+        paddle_2_pos = int(pot_galileo.ratio(to_min=MIN_Y, to_max=MAX_Y-PADDLE_SIZE))
 
         screen.addstr(ball_pos[1], ball_pos[0], ' ', curses.color_pair(2))
 
@@ -67,18 +77,18 @@ if __name__ == '__main__':
             # Check whether we collided with a paddle
             if not paddle_1_pos < ball_pos[1] < paddle_1_pos + PADDLE_SIZE:
                 ball_pos, ball_velocity = init_ball()
-       
+
         # If right border collision, increase score and invert vx
         if ball_pos[0] >= MAX_X:
             ball_velocity[0] = - ball_velocity[0]
             # Check whether we collided with a paddle
             if not paddle_1_pos < ball_pos[1] < paddle_1_pos + PADDLE_SIZE:
                 ball_pos, ball_velocity = init_ball()
-        
+
         # If top or botton collision, invert vy
         if not MIN_Y < ball_pos[1] < MAX_Y:
             ball_velocity[1] = - ball_velocity[1]
-        
+
         ball_pos = new_ball_pos(ball_pos, ball_velocity)
 
         screen.addstr(ball_pos[1], ball_pos[0], ' ', curses.color_pair(1))
@@ -86,7 +96,7 @@ if __name__ == '__main__':
         draw_paddle(MAX_X, paddle_2_pos, curses.color_pair(1))
 
         screen.refresh()
-        
+
         time.sleep(0.05)
-        
-        
+
+
