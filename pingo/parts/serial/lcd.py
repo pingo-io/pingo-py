2202f4ea74c2d45aa42a3c80fa692a157084d5aa
Luciano Ramalho <luciano@ramalho.org>
2015-05-14 16:46:31 -0300

    added repr to LCD16x2

--- pingo/parts/serial/lcd.py
+++ pingo/parts/serial/lcd.py
@@ -5,18 +5,21 @@ SPECIAL_CMD = b'\x7C'
 MOVE_CMD = b'\xFE'
 
 class LCD16x2(object):
-    
+
     def __init__(self, port, baudrate=9600):
         self.serial = serial.Serial(port, baudrate)
-        
+
     def clear(self):
         self.serial.write(SPECIAL_CMD + b'\x01')
-    
+
     def set_cursor(self, line=0, column=0):
         first_pos = (128, 192)
         code = first_pos[line] + column
         self.serial.write(MOVE_CMD + chr(code))
-        
+
     def write(self, octets):
         self.serial.write(octets)
-        
+
+    def __repr__(self):
+        cls_name = self.__class__.__name__
+        return '<{} at {!r}>'.format(cls_name, self.serial.port)
