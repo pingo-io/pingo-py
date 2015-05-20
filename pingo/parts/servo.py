5fcaa5d514f8bc168b2561e1754ea63b1108aa5a
Vido <vido@usp.br>
2015-05-20 11:14:05 -0300

    Non-Hardcode wave lenght parameters

--- pingo/parts/servo.py
+++ pingo/parts/servo.py
@@ -4,16 +4,19 @@ from pingo.board import ArgumentOutOfRange
 
 class Servo(object):
 
-    def __init__(self, pin, start_position=90):
+    def __init__(self, pin):
         pin.mode = pingo.PWM
         pin.frequency = 50
         self.pin = pin
-        self.move(start_position)
+        # Percent of dutycycle that represents zero
+        # Default: 2.5% ~ 0.5ms pulse @ 50Hz
+        self._botton_end = 2.5
+        # Percent of dutycycle that represents 180
+        # Default: 12.5% ~ 2.5ms pulse @ 50Hz
+        self._top_end = 12.5
 
     def move(self, position):
         if not 0 <= position <= 180:
             raise ArgumentOutOfRange()
-        self.pin.value = float(position) * (12.5 - 2.5) / (180.0) + 2.5
-
-
-
+        pmin, pmax = self._botton_end, self._top_end
+        self.pin.value = float(position) * (pmax - pmin) / (180.0) + pmin
