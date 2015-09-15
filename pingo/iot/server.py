20c86127502b4b4d87db10960eeec2102d7500ce
Luiz Menezes <luiz.menezesf@gmail.com>
2015-09-14 23:29:18 -0300

    Untested (v0.1) implmentation of http board access (#68).

---
+++ pingo/iot/server.py
@@ -0,0 +1,73 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from pingo.board import AnalogInputCapable
+import pingo
+
+
+app = Flask(__name__)
+api = Api(app)
+
+board = pingo.detect.get_board()
+
+
+class Main(Resource):
+    def get(self):
+        return {
+            'board': repr(board),
+            'pins': str(board.pins)
+        }
+
+
+class DigitalPins(Resource):
+    def get(self, input_type):
+        pins = board.pins()
+        return {'pins': str(pins)}
+
+
+class AnalogPins(Resource):
+    def get(self, input_type):
+        if issubclass(type(board), AnalogInputCapable):
+            pins = str(board.digital_pins)
+        else:
+            pins = []
+        return {'pins': str(pins)}
+
+
+class Input(Resource):
+    def get(self, input_type, pin):
+        pin = board.pins[pin]
+        pin.mode = pingo.IN
+        return {'input': pin.state}
+
+
+class AnalogOutput(Resource):
+    def get(self, output_type, pin, signal):
+        pin = board.pins[pin]
+        pin.mode = pingo.OUT
+        pin.value = signal
+        return {'output': signal}
+
+
+class DigitalOutput(Resource):
+    def get(self, output_type, pin, signal):
+        pin = board.pins[pin]
+        pin.mode = pingo.OUT
+        if signal:
+            pin.high()
+        else:
+            pin.low()
+        return {'output': signal}
+
+
+api.add_resource(Main, '/')
+api.add_resource(AnalogPins, '/analog')
+api.add_resource(DigitalPins, '/digital')
+api.add_resource(Input, '/<string:input_type>/<string:pin>')
+api.add_resource(AnalogOutput,
+                 '/<string:output_type>/analog/<string:pin>/<float:signal>')
+api.add_resource(DigitalOutput,
+                 '/<string:output_type>/digital/<string:pin>/<int:signal>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
