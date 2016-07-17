8ed7520b88407dfbde87f312dc51857eb9c6dbdf
Wellington Moraes <wellpunk@gmail.com>
2016-07-17 14:29:20 -0300

    add pushbutton led example

---
+++ pingo/examples/pushbutton_led/pushbutton_led.py
@@ -0,0 +1,29 @@
+"""Pushbutton led.
+
+The led comes on when you press the button.
+
+Connections example found on ./button.png
+
+"""
+# -*- coding: utf-8 -*-
+import pingo
+import sys
+
+
+try:
+    print("Loading board...")
+    board = pingo.detect.get_board()
+    print("Its ok...")
+    led_pin = board.pins[13]
+    led_pin.mode = pingo.OUT
+    button_pin = board.pins[2]
+    button_pin.mode = pingo.IN
+except Exception as e:
+    print("Error on get_board: {}".format(e))
+    sys.exit(1)
+
+while True:
+    if button_pin.state == pingo.HIGH:
+        led_pin.hi()
+    else:
+        led_pin.lo()
